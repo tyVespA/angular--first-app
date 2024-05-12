c0ba971dd66bef414716001a75c4fdde6de26fb4
tyVespA <migliavacca.marco08@gmail.com>
2024-05-12 23:37:45 +0200

    Complete lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<h1>Hello world!</h1>`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular--first-app';
+  title = 'homes';
 }
